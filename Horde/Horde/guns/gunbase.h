8f0ccc8b9ed743592a2277072ade51ee0d24e2e6
Sam Oates <sam@samoatesgames.com>
2012-12-14 17:41:42 +0000

    Started adding bullets and guns

--- Horde/Horde/guns/gunbase.h
+++ Horde/Horde/guns/gunbase.h
@@ -1,20 +1,26 @@
 #ifndef GUNBASE_H_INCLUDED
 #define GUNBASE_H_INCLUDED
 
+#include <vector>
+#include "bulletbase.h"
+
 class CGunBase {
 protected:
 
+	std::vector<CBulletBase>		m_bullet;
+
 public:
-                            //!
-                            CGunBase() {};
+									//!
+									CGunBase() {};
 
-                            //!
-                            ~CGunBase() {};
+									//!
+									~CGunBase() {};
 
-                            //!
-    void                    Shoot() {}
+									//!
+    virtual void					Shoot();
 
-    void                    Render() {};
+									//!
+    virtual void					Render();
 
 };
 
