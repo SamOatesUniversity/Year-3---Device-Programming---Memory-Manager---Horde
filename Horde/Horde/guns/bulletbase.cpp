623f4d1f667bdb49c4702d985fed902e952cc5e5
samoatesgames <samoatesgames@b333498f-789d-4143-b5e6-16d6a6d9a668>
2013-01-03 19:18:38 +0000

    Positioned bullet a little better... still not perfect.

--- Horde/Horde/guns/bulletbase.cpp
+++ Horde/Horde/guns/bulletbase.cpp
@@ -23,11 +23,15 @@ void CBulletBase::Fire(
 {
 	TVector<float, 2> startPosition = gun->GetPosition();
 	TVector<float, 2> direction = gun->GetDirection();
+	const int rotation = gun->GetRotation();
+
+	startPosition.Set(startPosition.x() + 24, startPosition.y() + 24);	// position at center of player
+	//startPosition.Set(startPosition.x() - (16 * cos(rotation * 0.017f)), startPosition.y() - (16 * sin(rotation * 0.017f)));	// need to offset onto gun somehow
 
 	m_position.Set(startPosition.x(), startPosition.y());
 	m_direction.Set(direction.x(), direction.y());
 
-	m_enity->SetRotation(gun->GetRotation());
+	m_enity->SetRotation(rotation);
 	m_enity->SetVisible(true);
 }
 
