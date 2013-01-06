c05ed8ff4942f4452797bf265b2b57b59f72a379
samoatesgames <samoatesgames@b333498f-789d-4143-b5e6-16d6a6d9a668>
2013-01-06 16:53:07 +0000

    Improved performance of memory manager when re-using free nuggets. Comments and style fixes.

--- !Amnesia/NotAmnesia.h
+++ !Amnesia/NotAmnesia.h
@@ -11,72 +11,111 @@
     #define nullptr 0
 #endif
 
+// We can remove most debug information and change how memory is allocated, by using this flag
+// vastly improving performance.
 #define OPTIMIZED
+
+// We have to allocated memory for our memory nuggets using a NEW and DELETE define.
+// The reason for this, is that malloc will not instanciate an instance of std::string
+// resulting in a crash.
+#ifndef OPTIMIZED
+	#define NEW(TYPE)	new TYPE; 
+	#define DELETE(OBJECT)	delete OBJECT;
+#else
+	#define NEW(TYPE)	static_cast<TYPE*>(malloc(sizeof(TYPE)))
+	#define DELETE(OBJECT)	free(OBJECT);
+#endif
 
 class CNotAmnesia : public IMemoryManager
 {
     private:
 
+	//! A doubly linked list of memory nuggets, which are used
+	//! to represent memory allocations
     struct MemoryNugget
     {
-        size_t                                      totalSize;
-        unsigned char								*ptr;
+        size_t                                      totalSize;									//! The total size of the memory nugget
+        unsigned char								*ptr;										//! The address the memory nugget represents
 
 #ifndef OPTIMIZED
-        std::string                                 file;
-        int                                         line;
+        std::string                                 file;										//! The file where the memory nugget was created
+        int                                         line;										//! The line of the file where the memory nugget was created
 #endif
 
-        MemoryNugget                                *nextNugget;
-        MemoryNugget                                *prevNugget;
+        MemoryNugget                                *nextNugget;								//! The previous memory nugget in the linked list
+        MemoryNugget                                *prevNugget;								//! The next memory nugget in the linked list
     };
 
     private:
 
         unsigned char                               *m_startPtr;                                //! The start point of the allocated memory (nullptr if uninitialized)
-        unsigned char                               *m_nextFreePtr;                             //!
+        unsigned char                               *m_nextFreePtr;                             //! A pointer of the next cached free memory address
 
-        MemoryNugget                                *m_lastNugget;                              //!
-		MemoryNugget                                *m_freeNugget;                              //!
+        MemoryNugget                                *m_lastNugget;                              //! A pointer to the last allocated memory nugget
+		MemoryNugget                                *m_freeNugget;                              //! A pointer to the last free memory nugget
 
 #ifndef OPTIMIZED
-		int											m_noofNuggets;
-		int											m_noofFreeNuggets;
+		int											m_noofNuggets;								//! The number of allocated memory nuggets
+		int											m_noofFreeNuggets;							//! The number of nuggets that have been moved to the free list of memory nuggets
 #endif
 
-        size_t                                      m_totalSize;                                //! The size of the allocated memory
-        size_t                                      m_amountAllocated;                          //! The size of the allocated memory
+        size_t                                      m_totalSize;                                //! The total size of the allocated memory
+        size_t                                      m_amountAllocated;                          //! The amount of memory we have allocated into memory nuggets
 
     private:
 
+													//! Find a memory nugget based upon a given memory address
+													//! \return THe memory nugget that represents the given address, or nullptr if no memory nugget was found
         MemoryNugget                                *FindMemeoryNugget(
-                                                        void *address
+                                                        void *address							//!< The address to find
                                                     );
 
+													//! Try to merge memory nuggets together to create a larger memory nugget
+													//! \return A memory nugget created from merging other nuggets, or nullptr on fail
+		MemoryNugget								*MergeMemoryNuggets(
+														size_t requiredSize						//!< The size we are looking to create a memory nugget
+													);
+
     public:
-                                                    //!
+                                                    //! Class constructor
                                                     CNotAmnesia();
 
-                                                    //!
+                                                    //! Class destructor
         virtual                                     ~CNotAmnesia();
 
-                                                    //!
-        bool                                        Initialise(size_t numBytes, void *preallocatedBuffer=0);
+                                                    //! Initializes the memory manger, allocating a given number of bytes
+													//! \return True on success, false on fail
+        virtual bool			                    Initialise(
+														size_t numBytes,						//!< The number of bytes to allocate
+														void *preallocatedBuffer = nullptr		//!< A pre-allocated buffer, or nullptr to create a new buffer
+													);
 
-                                                    //!
-        void                                        *Allocate(size_t numBytes, const char* file, int line);
+                                                    //! Allocate memory from our buffer, storing it in a memory nugget
+													//! \return The address of the allocated memory
+        virtual void                                *Allocate(
+														size_t numBytes,						//!< The number of bytes to allocate
+														const char* file,						//!< The file where the allocation was called
+														int line								//!< The line of the file where the allocation was called
+													);
 
-                                                    //!
-        void                                        *AllocateAligned(size_t numBytes,size_t alignment,const char* file, int line);
+                                                    //! NOT IMPLEMENTED
+        virtual void                                *AllocateAligned(
+														size_t numBytes,
+														size_t alignment,
+														const char* file, 
+														int line
+													);
 
-                                                    //!
-        void                                        Release(void* address);
+                                                    //! Release a given memory address, moving the memory nugget to the list of free memory nuggets for reuse
+        virtual void                                Release(
+														void* address							//!< The memory address to release
+													);
 
-                                                    //!
-        void                                        Shutdown();
+                                                    //! Release all memory and destroy all memory nuggets
+        virtual void                                Shutdown();
 
-                                                    //!
-        const char* const                           GetTextLine();
+                                                    //! NOT IMPLEMENTED
+        virtual const char* const                   GetTextLine();
 
 };
 
