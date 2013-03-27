e1f2b4e8fecd9a6940b610d82609ed273ffdb36c
samoatesgames <samoatesgames@b333498f-789d-4143-b5e6-16d6a6d9a668>
2013-03-27 16:29:13 +0000

    The game should reside in its own directory. Associated data files should be in a directory named ‘Data’ which should be in a subdirectory.

--- Horde-Optimized/Horde/guns/gunmachinegun.cpp
+++ Horde-Optimized/Horde/guns/gunmachinegun.cpp
@@ -15,11 +15,11 @@ void CGunMachinegun::Create()
 {
 	TVector<int, 2> frameLayout;
 	frameLayout.Set(8, 2);
-	m_entity = CLasagne::GetInstance()->LoadAnimatedImage("./media/graphics/characters/guns/machinegun/gun.png", frameLayout, 5);
+	m_entity = CLasagne::GetInstance()->LoadAnimatedImage("./data/graphics/characters/guns/machinegun/gun.png", frameLayout, 5);
 	if (!m_entity)
 		return;
 
-	m_audio = CLasagne::GetInstance()->LoadAudioFile("./media/sound/guns/machinegun.wav");
+	m_audio = CLasagne::GetInstance()->LoadAudioFile("./data/sound/guns/machinegun.wav");
 
 	static const int NoofBullets = 45;
 	for (int bulletIndex = 0; bulletIndex < NoofBullets; ++bulletIndex)
--- Horde-Optimized/Horde/guns/gunpistol.cpp
+++ Horde-Optimized/Horde/guns/gunpistol.cpp
@@ -14,11 +14,11 @@ void CGunPistol::Create()
 {
 	TVector<int, 2> frameLayout;
 	frameLayout.Set(8, 2);
-	m_entity = CLasagne::GetInstance()->LoadAnimatedImage("./media/graphics/characters/guns/pistol/gun.png", frameLayout, 5);
+	m_entity = CLasagne::GetInstance()->LoadAnimatedImage("./data/graphics/characters/guns/pistol/gun.png", frameLayout, 5);
 	if (!m_entity)
 		return;
 
-	m_audio = CLasagne::GetInstance()->LoadAudioFile("./media/sound/guns/pistol.wav");
+	m_audio = CLasagne::GetInstance()->LoadAudioFile("./data/sound/guns/pistol.wav");
 
 	static const int NoofBullets = 5;
 	for (int bulletIndex = 0; bulletIndex < NoofBullets; ++bulletIndex)
